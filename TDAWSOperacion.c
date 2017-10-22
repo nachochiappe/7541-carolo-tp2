4c0e65acc7e549eadef429e48aec26584ad5a86f
Ignacio Chiappe <nacho.chiappe@gmail.com>
2017-10-22 10:53:21 -0300

    Se declaran primitivas de TDAWSOperacion

--- TDAWSOperacion.c
+++ TDAWSOperacion.c
@@ -5,4 +5,38 @@
  *      Author: Yoel
  */
 
+void parserXML(TDAWS *ws) {
 
+}
+
+void parserJSON(TDAWS *ws) {
+
+}
+
+void getTime(TDAWS *ws) {
+
+}
+
+void getClientById(TDAWS *ws) {
+
+}
+
+void getMaxIdClient(TDAWS *ws) {
+
+}
+
+void setMaxIdClient(TDAWS *ws) {
+
+}
+
+void getAllClients(TDAWS *ws) {
+
+}
+
+void getAllOperations(TDAWS *ws) {
+
+}
+
+void validateOperation(TDAWS *ws) {
+
+}
--- TDAWSOperacion.h
+++ TDAWSOperacion.h
@@ -18,3 +18,21 @@ typedef struct TDAWSOperacion
 } TDAWSOperacion;
 
 #endif /* TDAWSOPERACION_H_ */
+
+void parserXML(TDAWS *ws);
+
+void parserJSON(TDAWS *ws);
+
+void getTime(TDAWS *ws);
+
+void getClientById(TDAWS *ws);
+
+void getMaxIdClient(TDAWS *ws);
+
+void setMaxIdClient(TDAWS *ws);
+
+void getAllClients(TDAWS *ws);
+
+void getAllOperations(TDAWS *ws);
+
+void validateOperation(TDAWS *ws);
