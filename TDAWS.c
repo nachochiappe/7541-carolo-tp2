373d4345eba9c7e26f249a60e53a6319c14e597a
Ignacio Chiappe <nacho.chiappe@gmail.com>
2017-10-22 10:13:44 -0300

    Modificaciones en TDAWS_Crear() y TDAWS_OperacionValida().

--- TDAWS.c
+++ TDAWS.c
@@ -6,6 +6,7 @@
  */
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 #include "cola.h"
@@ -16,51 +17,7 @@
 
 #define path_config "SERVERTP2GRUPAL.conf"
 
-int TDAWS_Crear(TDAWS * ws, char * cmd) {
-
-	/*
-	char tipo_op = 0; // 0 = POST; 1 = GET
-	char tipo_formato = 0; // 0 = JSON; 1 = XML
-	unsigned char i = 0;
-	if (strcmp(argv[i], "-X") == 0) {
-		// Verifico si es GET o POST
-		i++;
-		if (strcmp(argv[i], "GET") == 0)
-			tipo_op++;
-		else if (strcmp(argv[i], "POST") != 0) return -1;
-
-		i++;
-		if (strcmp(argv[i], "-H") == 0) {
-			// Verifico si es JSON o XML
-			i++;
-			if (tipo_op == 0) {
-				if (strcmp(argv[i], "\"Content-Type:application/json\"") == 0)
-					tipo_formato++;
-				else if (strcmp(argv[i], "\"Content-Type:application/xml\"") != 0) return -1;
-			}
-			else {
-				if (strcmp(argv[i], "\"Accept:application/json\"") == 0)
-					tipo_formato++;
-				else if (strcmp(argv[i], "\"Accept:application/xml\"") != 0) return -1;
-			}
-
-			// Obtengo URL donde consumir el servicio
-			i++;
-
-
-			// Parseo la data
-			i++;
-			if (strcmp(argv[i], "-d") == 0) {
-				// parsear mensaje
-				i++;
-			}
-			else return -1;
-		}
-		else return -1;
-
-	}
-	else return -1;
-	*/
+int TDAWS_Crear(TDAWS *ws, char *cmd) {
 
 	// OBTENER VALORES DE ARCHIVO DE CONFIGURACION
 
@@ -74,7 +31,7 @@ int TDAWS_Crear(TDAWS * ws, char * cmd) {
 	FILE *arch_config = fopen(path_config,"r");
 
 	for (unsigned int i = 0; i < 3; i++) {
-		fgets(str, 50, arch_config);
+		fgets(str, sizeof(str), arch_config);
 		token = strtok(str, "=");
 		if (strcmp(token, "pathOperaciones") == 0) {
 			path_operaciones = strtok(NULL, "=");
@@ -96,17 +53,18 @@ int TDAWS_Crear(TDAWS * ws, char * cmd) {
 	// LISTA DE OPERACIONES DISPONIBLES
 
 	ls_Crear(&ws->LOperaciones, 20);
-	char operacion[20];
+	char nombre_operacion[20];
 	FILE *arch_operaciones = fopen(path_operaciones,"r");
-	while (fgets(operacion, 20, arch_operaciones) != NULL)
-		ls_Insertar(&ws->LOperaciones, LS_SIGUIENTE, operacion);
+	while (fgets(nombre_operacion, sizeof(nombre_operacion), arch_operaciones) != NULL)
+		ls_Insertar(&ws->LOperaciones, LS_SIGUIENTE, nombre_operacion);
 	fclose(arch_operaciones);
 
 	// LISTA DE TDAClientes
 
 	ls_Crear(&ws->TClientes, sizeof(TElemCliente));
 	TElemCliente *cliente = (TElemCliente*) malloc(sizeof(TElemCliente));
-	char *linea_cliente;
+	if (!cliente) return -1;
+	char linea_cliente[255];
 	FILE *arch_clientes = fopen(path_clientes,"r");
 	while (fgets(linea_cliente, sizeof(TElemCliente), arch_clientes) != NULL) {
 		token = strtok(linea_cliente, ";");
@@ -127,20 +85,80 @@ int TDAWS_Crear(TDAWS * ws, char * cmd) {
 
 	// COLA CON LAS OPERACIONES EJECUTADAS
 
+	C_Crear(&ws->CEjecucion, sizeof(TDAWSOperacion));
+	TDAWSOperacion *operacion = (TDAWSOperacion*) malloc(sizeof(TDAWSOperacion));
+	if (!operacion) return -1;
+
+	/*
+	char tipo_op = 0; // 0 = POST; 1 = GET
+	char tipo_formato = 0; // 0 = JSON; 1 = XML
+	unsigned char i = 0;
+	if (strcmp(argv[i], "-X") == 0) {
+		// Verifico si es GET o POST
+		i++;
+		if (strcmp(argv[i], "GET") == 0)
+			tipo_op++;
+		else if (strcmp(argv[i], "POST") != 0) return -1;
+
+		i++;
+		if (strcmp(argv[i], "-H") == 0) {
+			// Verifico si es JSON o XML
+			i++;
+		if (tipo_op == 0) {
+			if (strcmp(argv[i], "\"Content-Type:application/json\"") == 0)
+				tipo_formato++;
+			else if (strcmp(argv[i], "\"Content-Type:application/xml\"") != 0) return -1;
+		}
+		else {
+			if (strcmp(argv[i], "\"Accept:application/json\"") == 0)
+				tipo_formato++;
+			else if (strcmp(argv[i], "\"Accept:application/xml\"") != 0) return -1;
+		}
+
+		// Obtengo URL donde consumir el servicio
+		i++;
+
+
+		// Parseo la data
+		i++;
+		if (strcmp(argv[i], "-d") == 0) {
+			// parsear mensaje
+			i++;
+		}
+		else return -1;
+	}
+	else return -1;
+
+	}
+	else return -1;
+	*/
+
 	return 0;
 }
 
-int TDAWS_OperacionValida(TDAWS * ws) {
+int TDAWS_OperacionValida(TDAWS *ws) {
+	char operacion_valida = 0;
+	char nombre_operacion[20];
+	ls_ElemCorriente(ws->LOperaciones, nombre_operacion);
+	while (operacion_valida == 0) {
+		if (strcmp(ws->TOperacion.dOperacion, nombre_operacion) == 0) {
+			operacion_valida = 1;
+		}
+		else {
+			if(ls_MoverCorriente(&ws->LOperaciones, LS_SIGUIENTE) != 1) {
+				return -1;
+			}
+		}
+	}
+	return 0;
+}
+
+int TDAWS_Consumir(TDAWS *ws) {
 
 	return 0;
 }
 
-int TDAWS_Consumir(TDAWS * ws) {
-
-	return 0;
-}
-
-int TDAWS_Destruir(TDAWS * ws) {
+int TDAWS_Destruir(TDAWS *ws) {
 
 	return 0;
 }
